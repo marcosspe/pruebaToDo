23ed887f8b21c1917b5afc90b807fbea24190740
marcosspe <marcos.perez.garcia0@gmail.com>
2021-05-10 01:43:02 +0000

    prueba

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,55 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState, useEffect } from "react";
 
 //create your first component
 export function Home() {
+	const [todoList, setTodoList] = useState([]);
+
+	useEffect(() => {
+		getData();
+	}, []);
+
+	const handleKeyPress = e => {
+		if (e.target.value !== "" && e.charCode === 13) {
+			let newTodo = {
+				label: e.target.value,
+				done: false
+			};
+			let newTodoList = [...todoList, newTodo];
+			setTodoList(newTodoList);
+		}
+	};
+	const getData = () => {
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/marcosspe")
+			.then(resp => resp.json())
+			.then(data => setTodoList(data))
+			.catch(error => console.log(error));
+	};
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container-fluid">
+			<div className="row d-flex justify-content-center">
+				<div className="col-12 col-md-6 col-xl-4">
+					<div className="card mt-5" style={{ width: "100%" }}>
+						<input
+							onKeyPress={handleKeyPress}
+							placeholder="Agregar Tarea"
+						/>
+						<ul className="list-group list-group-flush">
+							{todoList.length === 0 ? (
+								<li className="list-group-item">
+									Cargando...{" "}
+								</li>
+							) : (
+								todoList.map((todo, index) => (
+									<li key={index} className="list-group-item">
+										{todo.label}
+									</li>
+								))
+							)}
+						</ul>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }
